a2ad717883fdfab3fff32f69b727ef5f2a636fa0
Mauricio Lima <git@mauriciolima.eti.br>
2021-05-23 05:33:49 -0300

    Problem #1 solution source

--- sources/rdi-problems-set/problem_1/problem_1.cpp
+++ sources/rdi-problems-set/problem_1/problem_1.cpp
@@ -9,8 +9,61 @@ using namespace std;
 
 // Complete the timeInWords function below.
 string timeInWords(int h, int m) {
+    string numbers[] = { "midnight", "one",       "two",      "three"  ,  "four"   ,
+                                     "five",      "six",      "seven"  ,  "eight"  ,
+                                     "nine",      "ten",      "eleven" ,  "twelve" ,
+                                     "thrirteen", "fourteen", "fifteen",  "sixteen",
+                                     "seventeen", "eighteen", "nineteen"
+    };
+    string result;
 
-    return "";
+    try
+    {
+        if ((h < 0) || (h > 12))
+            throw "Invalide time value";
+
+        if ((m < 0) || (m > 59))
+            throw "Invalid minutes value";
+
+        result = numbers[h];
+
+        switch (m)
+        {
+        case 0:
+            return result + "o' clock";
+
+        case 15:
+            return "quarter past " + result;
+
+        case 30:
+            return "half past " + result;
+
+        case 45:
+            return "quarter to " + result;
+
+        }
+
+        if (m > 30) {
+            m = 60 - m;
+            result = " to " + numbers[h + 1];
+        }
+        else {
+            result = " past " + numbers[h];
+        }
+
+        if (m < 20)
+            return numbers[m] + result;
+
+        if (m >= 20)
+            return "twenty" + (m > 20 ? " " + numbers[m % 20] : "") + result;
+
+    }
+    catch (string e)
+    {
+        throw e;
+    }
+
+    return result;
 }
 
 
