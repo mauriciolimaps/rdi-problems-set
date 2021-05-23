812000e0d77db3a56a0b162acf66bea420cee817
Mauricio Lima <>
2021-05-23 16:41:00 +0000

    Update test scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
 
+function divider()
+{
+	echo --------------------------------------------------
+}
+
+function problem_1_test()
+{
+	echo
+	output=OUTPUT1.TXT
+	rm  $output
+	echo Input:
+	cat $1 
+	cat $1 | ../bin/Debug/problem_1
+	echo
+	echo Output:
+	cat $output
+	echo
+
+}
+
 cd tests
-cat problem-1-01.txt | ../bin/Debug/problem_1
+
+echo \##  Problem 1 \#####################################
+problem_1_test problem-1-01.txt
+divider
+problem_1_test problem-1-01.txt
+
+echo
+echo \##  Problem 2 \#####################################
+
