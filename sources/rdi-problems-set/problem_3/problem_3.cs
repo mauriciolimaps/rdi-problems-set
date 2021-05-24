5e662c832376c44fe834de67966cf01a00a8cd79
Mauricio Lima <git@mauriciolima.eti.br>
2021-05-24 10:32:30 -0300

    Introducing 'Problem #3' project skeleton

---
+++ sources/rdi-problems-set/problem_3/problem_3.cs
@@ -0,0 +1,49 @@
+﻿namespace problem_3
+{
+    using System.CodeDom.Compiler;
+    using System.Collections.Generic;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
+    using System.Text;
+    using System;
+
+    class Result
+    {
+
+        // Complete the 'pickingNumbers' function below.
+
+        public static int pickingNumbers(List<int> a)
+        {
+
+
+            return 0;
+        }
+    }
+
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter("OUTPUT3.TXT", true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+            int result = Result.pickingNumbers(a);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
